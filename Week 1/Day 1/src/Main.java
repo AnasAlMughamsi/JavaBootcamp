cb859213d447ceba033d6295bd6ab3aaa5699b3e
Anas <Anas.Mughamsi@gmail.com>
2023-01-09 17:27:37 +0300

    add day2

--- Week 1/Day 1/src/Main.java
+++ Week 1/Day 1/src/Main.java
@@ -16,16 +16,51 @@ public class Main {
 
         // conditions
 
-        String role;
-        System.out.print("Enter your role: ");
-        role = input.nextLine();
+//        String role;
+//        System.out.print("Enter your role: ");
+//        role = input.nextLine();
+//
+//        if(role.equals("Admin")) {
+//            System.out.println("Hello Admin");
+//        } else {
+//            System.out.println("Hello");
+//        }
 
-        if(role.equals("Admin")) {
-            System.out.println("Hello Admin");
+
+        System.out.print("Enter your grade: ");
+        int grade = input.nextInt();
+
+        if(grade <= 100 && grade >= 90) {
+            System.out.println("A");
+        } else if (grade <= 89 && grade >= 80) {
+            System.out.println("B");
+        } else if (grade <= 79 && grade >= 70) {
+            System.out.println("C");
+        } else if (grade <= 69 && grade >= 60) {
+            System.out.println("D");
         } else {
-            System.out.println("Hello");
+            System.out.println("F");
         }
 
 
+        // switch case
+
+        int number;
+        System.out.print("Enter 1 to check your balance");
+        System.out.print("Enter 2 to show your user name");
+        System.out.println("Enter 3 to call customer service");
+        number = input.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Your balance is 100,000");
+                break;
+            case 2:
+                System.out.println("Your user name is Anas");
+                break;
+            case 3:
+                System.out.println("Calling customer service...");
+                break;
+        }
     }
 }
